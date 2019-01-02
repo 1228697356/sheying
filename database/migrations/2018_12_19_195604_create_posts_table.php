6321e81f9c52b40c27cf19ab43740ef604439c60
yiming <1228697356@qq.com>
2019-01-02 19:12:19 +0800

    v1.0

---
+++ database/migrations/2018_12_19_195604_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id')->comment('主键id');
+            $table->string('title', 100)->default('')->comment('文章标题');
+            $table->text('content')->comment('文章内容');
+            $table->text('photo')->comment('文章内容');
+            $table->unsignedInteger('user_id')->default('0')->comment('用户id(外键)');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
